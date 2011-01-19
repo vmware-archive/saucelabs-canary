f825290be6345455b57e1a90a3931f44e08f6744
Chad Woolley <thewoolleyman@gmail.com>
2011-01-19 03:35:15 -0700

    fixes for rvm + cruise

--- cruise_build_command.sh
+++ cruise_build_command.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-source $HOME/.rvm/environments/1.8.7-p299@saucelabs-canary #  have to hardcode env to match the one in .rvmrc
-source .rvmrc
 export IS_CI_BOX=true
-rvm exec bundle install
-rvm exec rake $@
+bundle install
+rake selenium:ci
